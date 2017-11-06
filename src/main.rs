74cdd5c8711b29d636904799a8e8bbfa7c885434
Chris West (Faux) <git@goeswhere.com>
2017-11-06 07:16:34 +0000

    done

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
 extern crate iron;
 extern crate params;
-extern crate rand;
-extern crate router;
 
 use iron::prelude::*;
-use iron::status;
 use params::Params;
 
 fn handle(req: &mut Request) -> IronResult<Response> {
-    let params = req.get_ref::<Params>();
-    if params.is_err() {
-        return Ok(Response::with((status::BadRequest, "'not a form post'")));
-    }
-
+    req.get_ref::<Params>();
     unimplemented!()
 }
 
 fn main() {
-    let mut router = router::Router::new();
-    router.post("/", handle, "root");
-    Iron::new(router).http("127.0.0.1:9696").unwrap();
+    Iron::new(handle).http("127.0.0.1:9696").unwrap();
 }
