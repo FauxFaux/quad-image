e3a3f74aba142e30f392e74159a5cf0d0f1899f2
Chris West (Faux) <git@goeswhere.com>
2017-11-06 07:12:09 +0000

    pretty simple

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,12 @@ extern crate iron;
 extern crate params;
 extern crate rand;
 extern crate router;
-extern crate tempfile;
-
-use std::fs;
-use std::io;
-use std::io::BufRead;
-use std::io::Seek;
-use std::io::SeekFrom;
 
 use iron::prelude::*;
 use iron::status;
 use params::Params;
 
-use rand::Rng;
-
-const C_OPEN_FAILED_ALREADY_EXISTS: i32 = 17;
-
-fn store(f: &params::File) -> io::Result<String> {
-    unimplemented!()
-}
-
-fn upload(req: &mut Request) -> IronResult<Response> {
+fn handle(req: &mut Request) -> IronResult<Response> {
     let params = req.get_ref::<Params>();
     if params.is_err() {
         return Ok(Response::with((status::BadRequest, "'not a form post'")));
@@ -33,6 +18,6 @@ fn upload(req: &mut Request) -> IronResult<Response> {
 
 fn main() {
     let mut router = router::Router::new();
-    router.post("/api/upload", upload, "upload");
-    Iron::new(router).http("127.0.0.1:6699").unwrap();
+    router.post("/", handle, "root");
+    Iron::new(router).http("127.0.0.1:9696").unwrap();
 }
