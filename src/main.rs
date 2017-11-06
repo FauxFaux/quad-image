eb894c0a8c898b050b771df4fc735325b708c3b2
Chris West (Faux) <git@goeswhere.com>
2017-11-06 07:09:04 +0000

    that's simpler than I expected

--- src/main.rs
+++ src/main.rs
@@ -18,86 +18,17 @@ use rand::Rng;
 
 const C_OPEN_FAILED_ALREADY_EXISTS: i32 = 17;
 
-fn outfile(ext: &str) -> String {
-    let mut rand = rand::thread_rng();
-    loop {
-        let rand_bit: String = rand.gen_ascii_chars().take(10).collect();
-        let cand = format!("e/{}.{}", rand_bit, ext);
-        match fs::OpenOptions::new().write(true).create_new(true).open(
-            &cand,
-        ) {
-            Ok(_) => return cand,
-            Err(e) => {
-                // TODO: this probably panics on non-Linux(?).
-                // TODO: not a major problem as it's hard to hit anyway.
-                match e.raw_os_error() {
-                    Some(C_OPEN_FAILED_ALREADY_EXISTS) => {}
-                    _ => panic!(format!("couldn't create candidate {}: {:?}", cand, e)),
-                }
-            }
-        }
-    }
-}
-
-fn make_readable(path: &str) -> io::Result<()> {
-    let mut perms = fs::File::open(path)?.metadata()?.permissions();
-
-    use std::os::unix::fs::PermissionsExt;
-    perms.set_mode(0o0644);
-    fs::set_permissions(path, perms)
-}
-
 fn store(f: &params::File) -> io::Result<String> {
     unimplemented!()
 }
 
 fn upload(req: &mut Request) -> IronResult<Response> {
-    let host = req.headers
-        .get::<iron::headers::Host>()
-        .expect("host header present")
-        .hostname
-        .clone();
-    let remote_addr = req.remote_addr;
-    let remote_forwarded = req.headers.get_raw("X-Forwarded-For").map(|vecs| {
-        vecs.iter()
-            .map(|vec| {
-                String::from_utf8(vec.clone()).expect("valid utf-8 forwarded for")
-            })
-            .collect::<Vec<String>>()
-    });
     let params = req.get_ref::<Params>();
     if params.is_err() {
         return Ok(Response::with((status::BadRequest, "'not a form post'")));
     }
 
-    let params: &params::Map = params.unwrap();
-
-    match params.get("image") {
-        Some(&params::Value::File(ref f)) => {
-            match store(f) {
-                Err(e) => {
-                    println!("{:?} {:?}: failed: {:?}", remote_addr, remote_forwarded, e);
-                    Ok(Response::with(status::InternalServerError))
-                }
-                Ok(code) => {
-                    println!("{:?} {:?}: {}", remote_addr, remote_forwarded, code);
-                    let url = format!("https://{}/{}", host, code);
-                    let dest = iron::Url::parse(url.as_str()).expect("url 2");
-                    if params.contains_key("js-sucks") {
-                        Ok(Response::with((status::Ok, code)))
-                    } else {
-                        Ok(Response::with(
-                            (status::SeeOther, iron::modifiers::Redirect(dest)),
-                        ))
-                    }
-
-                }
-            }
-        }
-        _ => Ok(Response::with(
-            (status::BadRequest, "'image attr not present'"),
-        )),
-    }
+    unimplemented!()
 }
 
 fn main() {
