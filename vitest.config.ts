7f3362793a2dfd44922d2d62afd2a1f371530a8c
Chris West (Faux) <git@goeswhere.com>
2026-02-21 15:44:33 +0000

    chore: don't test compiled code

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, configDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
-  }
+    exclude: [...configDefaults.exclude, '.lint/**'],
+  },
 });
